b0f9c8c92fd9bb6ec48f7b83d33341dbd0f52359
Arman-Kabir <armankabir8@gmail.com>
2023-04-04 08:14:07 +0600

    scene,camera,renderer

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-import './style.css'
-import * as THREE from 'https://unpkg.com/three@0.149.0/build/three.module.js';
+import './style.css';
+
+//To install it from CDN 
+// import * as THREE from 'https://unpkg.com/three@0.149.0/build/three.module.js';
+
+import * as THREE from 'three';
 
 const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+
 console.log(scene);
+console.log(camera);
+console.log(renderer);
 
 
 
