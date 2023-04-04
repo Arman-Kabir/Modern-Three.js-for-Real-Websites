30124953d86c9e73efe04437233357cb1b375ad1
Arman-Kabir <armankabir8@gmail.com>
2023-04-04 14:49:29 +0600

    plane geometry

--- main.js
+++ main.js
@@ -9,14 +9,9 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 
+
+// Renderer
 const renderer = new THREE.WebGLRenderer();
-
-
-
-console.log(scene);
-console.log(camera);
-console.log(renderer);
-
 
 renderer.setSize(innerWidth, innerHeight);
 renderer.setPixelRatio(devicePixelRatio);
@@ -24,28 +19,31 @@ renderer.setPixelRatio(devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
 // Mesh = geometry + material
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-
-console.log(boxGeometry);
-console.log(material);
-
-const mesh = new THREE.Mesh(boxGeometry, material);
-console.log(mesh);
-
-scene.add(mesh);
+// const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const mesh = new THREE.Mesh(boxGeometry, material);
+// scene.add(mesh);
 
 camera.position.z = 5;
+
+// Plane Geo
+const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000,side:THREE.DoubleSide });
+
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(planeMesh);
+
+console.log(planeGeometry);
 
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    mesh.rotation.x += .01;
-    mesh.rotation.y += .01;
+    // mesh.rotation.x += .01;
+    // mesh.rotation.y += .01;
+
+    // planeMesh.rotation.x  +=.01;
+    // planeMesh.rotation.y  +=.01;
 }
-
-
 animate();
 
 
