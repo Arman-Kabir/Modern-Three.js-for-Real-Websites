f2751ead1673e1fd07b1964e2a4058fa7e0c603d
Arman-Kabir <armankabir8@gmail.com>
2023-04-04 14:06:54 +0600

    box geo

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import './style.css';
+import * as THREE from 'three';
 
 //To install it from CDN 
 // import * as THREE from 'https://unpkg.com/three@0.149.0/build/three.module.js';
 
-import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 
@@ -20,6 +20,23 @@ console.log(renderer);
 
 renderer.setSize(innerWidth, innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const boxGeometry = new THREE.BoxGeometry(1,1,1);
+
+const material = new THREE.MeshBasicMaterial({color:0x00ff00});
+
+console.log(boxGeometry);
+console.log(material);
+
+const mesh = new THREE.Mesh(boxGeometry,material);
+console.log(mesh);
+
+scene.add(mesh);
+
+camera.position.z = 5;
+
+renderer.render(scene,camera);
+
 
 
 
